9c51240097f46d8aed2899c93c7a0aecfbb1ef6d
Muni-M <111742416+Muniraja-M@users.noreply.github.com>
2023-01-16 08:39:46 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  const [state, setState] = useState(true);
+
+  const RegisterForm = (
+    <form id="Regform">
+      <input type="text" placeholder="username" />
+      <input type="E-mail" placeholder="Email" />
+      <input type="password" placeholder="password" />
+      <button type="submit" className="btn">
+        Register
+      </button>
+    </form>
+  );
+
+  const LoginForm = (
+    <form id="loginform">
+      <input type="text" placeholder="username" />
+      <input type="password" placeholder="password" />
+      <button type="submit" className="btn">
+        Login
+      </button>
+      <a href="#">Forgot password</a>
+    </form>
+  );
+
+  return (
+    <div className="acc-page">
+      <div className="form-container">
+        <div className="form-btn">
+          <span onClick={() => setState(false)}>Login</span>
+          <span onClick={() => setState(true)}>Register</span>
+          {state ? RegisterForm : LoginForm}
+        </div>
+      </div>
+    </div>
+  );
+}
